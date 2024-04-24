bb0b141635941eb3f5517f04b286cb195cf48b6b
Arthur Owens <arth.owens@gmail.com>
2024-04-24 20:51:34 +0000

    0x00. MySQL advanced

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- An SQL script listing all bands with Glam rock as their main style
 -- ranked by their longevity
-SELECT band_name, IFNULL(split, 2020) - formed AS lifespan
+SELECT band_name, 
+       YEAR('2022-01-01') - SUBSTRING_INDEX(formed, '-', 1) AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET("Glam rock", style)
 ORDER BY lifespan DESC;
